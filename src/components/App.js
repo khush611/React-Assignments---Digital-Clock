7217cb62ecdc3748d432b88c3472f9ef31bb0920
Khusboo Kumari <36759104+khush611@users.noreply.github.com>
2020-11-02 11:04:34 +0530

    added class App

--- src/components/App.js
+++ src/components/App.js
@@ -38,8 +38,10 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="Clock">
-        <h3 id="time">{this.state.time}</h3>
+      <div className="App">
+        <div className="Clock">
+          <h3 id="time">{this.state.time}</h3>
+        </div>
       </div>
     );
   }
