977c21a376ef0aa409f0f7afb870d6bcc370cefb
Khusboo Kumari <36759104+khush611@users.noreply.github.com>
2020-11-02 09:59:36 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,51 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-            <>
-               
-            </>
-        )
-    }
+  componentDidMount() {
+    this.update = setInterval(() => {
+      this.getTime();
+    });
+  }
+  constructor() {
+    super();
+    this.state = {
+      time: ""
+    };
+  }
+  componentWillUnmount() {
+    // delete the interval just before component is removed
+    clearInterval(this.update);
+  }
+  getTime() {
+    setInterval(() => {
+      let date = new Date();
+      let hour = date.getHours();
+      let minute = date.getMinutes();
+      let seconds = date.getSeconds();
+      let ampm = this.hour >= 12 ? "AM" : "PM";
+      hour = hour % 12;
+      hour = hour ? hour : 12;
+      hour = fullTime(hour);
+      minute = fullTime(minute);
+      seconds = fullTime(seconds);
+      this.setState({
+        time: (hour % 12) + ":" + minute + ":" + seconds + " " + ampm
+      });
+      function fullTime(n) {
+        return n < 10 ? "0" + n : n;
+      }
+    }, 1000);
+  }
+  render() {
+    return (
+      <>
+        <div className="Clock">
+          <h3 className="time">{this.state.time}</h3>
+        </div>
+      </>
+    );
+  }
 }
-
 
 export default App;
